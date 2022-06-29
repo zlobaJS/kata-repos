af3f3d085b1f8d041072de5edb982244952662cd
Владимир Арутюнов <zlobajs@yandex.ru>
2022-06-29 19:45:56 +0400

    Finaly project

--- modules/getIp.js
+++
@@ -1,9 +0,0 @@
-// Получаем ip
- const span = document.querySelector('span');
-
-export const getIp = async (url) => {
-    const res = await fetch(url);
-    const response = await res.json();
-    span.innerText = `IP: ${response.ipAddress} City: ${response.city} Continent: ${response.continentName}`;
-
-}
